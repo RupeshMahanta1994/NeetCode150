10b64d160e586ee2a19ad85f983ba1d014dcb922
rupesh-hub <mahanta.rupesh704@gmail.com>
2025-11-14 08:55:17 +0530

    Add implementations for Number of 1 bits, Counting Bits, and Missing Number

---
+++ Bit Manupulation/3. Counting Bits/code.js
@@ -0,0 +1,14 @@
+function contingBits(n) {
+  //create a dp array
+  let dp = new Array(n + 1).fill(0);
+  let offset = 1;
+  for (let i = 1; i <= n; i++) {
+    if (offset * 2 == i) {
+      offset = i;
+    }
+    dp[i] = 1 + dp[i - offset];
+  }
+  return dp;
+}
+let n = 5;
+console.log(contingBits(n));
