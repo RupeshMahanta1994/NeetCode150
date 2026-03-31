e41c097ffd3082eb9409d25a7e6acc79d02e968f
rupesh-hub <mahanta.rupesh704@gmail.com>
2026-03-31 11:12:52 +0530

    Add implementation to check if an array is sorted in Go

---
+++ Arrays/3. Check if array is sorted or not/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func sorted(nums []int) bool {
+	for i := 1; i < len(nums); i++ {
+		if nums[i] < nums[i-1] {
+			return false
+		}
+	}
+	return true
+}
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	result := sorted(nums)
+	fmt.Println(result)
+
+}
